c1714866f6579e334995b55ab4ff9edcdb3b4fcc
Leo Mengesha <barggros@gmail.com>
2026-06-09 15:28:53 +0100

    aesthetic changes.

--- Tiny.Jarvis/Tiny.Jarvis.Main/Program.cs
+++ Tiny.Jarvis/Tiny.Jarvis.Main/Program.cs
@@ -120,12 +120,12 @@ List<string> SelectTrainingFile(string pathToDir, List<string> files)
     AddFile(pathToDir, files, filesAvailable);
 
     var fetch = true;
-    var posibleResponses = new[] { "y", "Yes", "Y", "YES" };
+    var posibleResponses = new[] { "y", "Y", "n", "N" };
     while(fetch)
     {
-        
+
         Console.WriteLine(Environment.NewLine);
-        Console.Write($"Fetch Another ({string.Join(",", posibleResponses)}): ");
+        Console.Write($"Fetch Another ({string.Join("/", posibleResponses)}): ");
         var userResponseInput = Console.ReadLine()?.ToLower();
 
         fetch = userResponseInput == "y" || userResponseInput == "yes";
@@ -134,6 +134,7 @@ List<string> SelectTrainingFile(string pathToDir, List<string> files)
 
         else break;
     }
+    Console.WriteLine(Environment.NewLine);
 
     return files;
 }
